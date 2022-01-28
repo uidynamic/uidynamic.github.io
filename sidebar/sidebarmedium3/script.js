891520074bc473f7e23e3fe748584bada662ddc4
agusmahendraa <creativemahen@github.com>
2022-01-28 20:07:57 +0800

    update

--- sidebar/sidebarmedium3/script.js
+++ sidebar/sidebarmedium3/script.js
@@ -2,9 +2,6 @@ function openNav() {
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("trigger").style.display = "block";
     document.getElementById("navbar").style.backgroundColor = "#CCD0D5";
-
-    
-   
   }
   function closeNav() {
     document.getElementById("mySidebar").style.display = "none";
